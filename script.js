e5d59985977b50d5c476841c7725ce849b8bae14
irgi <irgiaditpratama@gmail.com>
2025-09-29 23:58:31 +0700

    '

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.querySelector('h1').textContent = 'Hello, Git!';   
 console.log('Script loaded.');
 document.querySelector('h2').textContent = 'Hello, Git!';
+document.querySelector('h3').textContent = 'Hello, Git!';
+document.querySelector('h4').textContent = 'Hello, Git!';
